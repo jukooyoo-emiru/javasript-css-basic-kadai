d1dcb8cf9789daaf8dd1b5a8778b0234c0d665ac
jukooyoo-emiru <emirchmd@gmail.com>
2026-06-16 20:33:04 +0900

    mdn.jsを変更

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,9 +1,9 @@
 const today = () => {
   const date = new Date();
-  const [month, day, year] = [
+  const [year, month, day] = [
+  date.getFullYear(),
   date.getMonth() + 1,
   date.getDate(),
-  date.getFullYear(),
 ]
   console.log(year + '年' + month + '月' + day + '日')
 }
